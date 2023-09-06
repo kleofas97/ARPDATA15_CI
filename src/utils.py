389a5660f739281592c46023a2f66433e1f8edba
Adam Mika <mika.adam@outlook.com>
2023-09-06 19:06:45 +0200

    added tests

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,14 @@
 class Calculator:
 
     def __init__(self, a, b):
-        self.a = a
-        self.b = b
+        if isinstance(a, (int,float)):
+            self._a = a
+        else:
+            raise Exception('wrong parameter a please use only int or float')
+        if isinstance(b, (int, float)):
+            self._b = b
+        else:
+            raise Exception('wrong parameter a please use only int or float')
 
     def dodawanie(self):
-        return self.a + self.b
+        return self._a + self._b
